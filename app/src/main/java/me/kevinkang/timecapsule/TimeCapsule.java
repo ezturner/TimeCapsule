bd636f62824122a5e5ca498c91cc4be7f366584a
Ethan Turner <eturner64@gmail.com>
2016-10-15 22:28:30 -0700

    Added some UI functionality

--- app/src/main/java/me/kevinkang/timecapsule/TimeCapsule.java
+++ app/src/main/java/me/kevinkang/timecapsule/TimeCapsule.java
@@ -1,10 +1,22 @@
 package me.kevinkang.timecapsule;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by Work on 10/15/2016.
  */
 
 public class TimeCapsule extends Application {
+
+    private static Context applicationContext;
+
+    public static Context getContext(){
+        return applicationContext;
+    }
+
+    @Override
+    public void onCreate(){
+        applicationContext = getContext();
+    }
 }
